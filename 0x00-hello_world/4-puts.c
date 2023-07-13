cb09a7bbacdc6e3ac0591e6ee8856546f01c935b
AbrahamAlgorithm <abrahamfolorunso6@gmail.com>
2023-07-13 10:11:52 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- *main entry point
+ * main - main entry point
+ *
+ * Return: 0
  */
- int main() {
+ int main(void)
+{
  puts("Programming is like building a multilingual puzzle");
  return 0;
- }
+}

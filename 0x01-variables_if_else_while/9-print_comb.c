8bfb06781b4ccd85a5f305f8bdab3c1ceb6f1903
AbrahamAlgorithm <abrahamfolorunso6@gmail.com>
2023-07-14 09:16:29 -0700

    magically

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

3c51a2089bfe369deb7b709b17e48fa22af09149
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-07-21 03:40:38 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - a function that multiplies two int
+ * a - int1
+ * b - int2
+ * Return : the result
+ */
+int mul(int a, int b);
+{
+	int num = a * b;
+
+	return (num);
+}

1185e09a25184faa74a9c46b36e6d11703c300b3
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-08-02 08:01:57 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion : returns the lenght of a string
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}

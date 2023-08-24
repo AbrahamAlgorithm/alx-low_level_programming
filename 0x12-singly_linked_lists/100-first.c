8f7576e670059aca0709c99a8e183650742d9df3
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-08-24 12:16:50 -0700

    The hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * prints a sentence
+ * main - execute
+ *
+ * Return : 0
+ */
+
+void __attribute__((constructor)) hare(void);
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}

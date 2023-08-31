0405c7b4a4d21f975a2198943e076a67d2f73954
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-08-31 11:03:20 -0700

    00000000000000000000000000

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * binary-int -converts binary numbers to a decimal
+ * @b - pointing to a string of 0 and 1
+ * Return: the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b);
+{
+	unsigned int dec = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b == '1')
+			dec = (dec << 1) | 1;
+		else if (*b == '0')
+			dec <<= 1;
+		else
+			return (0);
+		b++;
+	}
+
+	return (dec);
+}

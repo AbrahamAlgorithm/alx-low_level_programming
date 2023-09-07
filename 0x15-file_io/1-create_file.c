c6ea49b7e4e513b104e7e86fc40e02457b7ccbfc
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-09-07 16:24:26 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,61 @@
 #include "main.h"
 
+
 /**
-* create_file - a function that creates a file
-* @filename: the filename
-* @text_content: a NULL terminated string to write to the file
-* Return: 1 on success, -1 on failure
-**/
 
-int create_file(const char *filename, char *text_content)
+ * append_text_to_file - Appends text at the end of a file.
+
+ * @filename: A pointer to the name of the file.
+
+ * @text_content: The string to add to the end of the file.
+
+ *
+
+ * Return: If the function fails or filename is NULL - -1.
+
+ *         If the file does not exist the user lacks write permissions - -1.
+
+ *         Otherwise - 1.
+
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+
 {
-	int f;
-	int length, res;
 
-	if (filename == NULL)
-		return (-1);
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (f == -1)
-		return (-1);
+        int o, w, len = 0;
 
-	if (!text_content)
-		return (1);
-	length = 0;
-	while (text_content[length] != '\0')
-		length += 1;
 
-	res = write(f, text_content, length);
-	if (res != length)
-		return (-1);
-	res = close(f);
-	if (res == -1)
-		exit(-1);
+        if (filename == NULL)
 
-	return (1);
+                return (-1);
+
+
+        if (text_content != NULL)
+
+        {
+
+                for (len = 0; text_content[len];)
+
+                        len++;
+
+        }
+
+
+        o = open(filename, O_WRONLY | O_APPEND);
+
+        w = write(o, text_content, len);
+
+
+        if (o == -1 || w == -1)
+
+                return (-1);
+
+
+        close(o);
+
+
+        return (1);
+
 }
+

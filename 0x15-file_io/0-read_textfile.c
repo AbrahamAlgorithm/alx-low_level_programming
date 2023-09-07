bb07ae64dce0b2d4477074e72da925676a3b4796
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-09-07 16:38:15 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,54 @@
 #include "main.h"
-/**
-* read_textfile - a function that reads a text file
-*and prints it to the POSIX standard output
-* @filename: the file name
-* @letters: the number of letters
-* Return: the actual number of letters or 0 if fails
-**/
 
-int create_file(const char *filename, char *text_content)
+#include <stdlib.h>
+
+
+/**
+
+ * read_textfile- Read text file print to STDOUT.
+
+ * @filename: text file being read
+
+ * @letters: number of letters to be read
+
+ * Return: w- actual number of bytes read and printed
+
+ *        0 when function fails or filename is NULL.
+
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+
 {
-        int fd, w, len = 0;
-        if (filename == NULL)
-                return (-1);
-        if (text_content != NULL)
-        {
-                for (len = 0; text_content[len];)
-                    len++;
-}
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        w = write(fd, text_content, len);
-        if (fd == -1 || w == -1)
-                return (-1);
+
+        char *buf;
+
+        ssize_t fd;
+
+        ssize_t w;
+
+        ssize_t t;
+
+
+        fd = open(filename, O_RDONLY);
+
+        if (fd == -1)
+
+                return (0);
+
+        buf = malloc(sizeof(char) * letters);
+
+        t = read(fd, buf, letters);
+
+        w = write(STDOUT_FILENO, buf, t);
+
+
+        free(buf);
+
         close(fd);
-        return (1);
+
+        return (w);
+
 }
 
 

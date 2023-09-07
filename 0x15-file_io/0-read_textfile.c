c0113d70583f1fd7781fc73f34bb017fc56ee7df
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-09-07 16:22:59 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * read_textfile - a function that reads a text file
 *and prints it to the POSIX standard output
@@ -9,43 +8,21 @@
 **/
 
 int create_file(const char *filename, char *text_content)
-
 {
-
         int fd, w, len = 0;
-
-
         if (filename == NULL)
-
                 return (-1);
-
-
         if (text_content != NULL)
-
         {
-
                 for (len = 0; text_content[len];)
-
-                        len++;
-
-        }
-
-
+                    len++;
+}
         fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
         w = write(fd, text_content, len);
-
-
         if (fd == -1 || w == -1)
-
                 return (-1);
-
-
         close(fd);
-
-
         return (1);
-
 }
 
 

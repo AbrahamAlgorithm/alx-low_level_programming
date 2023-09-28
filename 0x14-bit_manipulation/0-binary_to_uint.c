45a706c54113d07ea8c3411f59628d87a7db606f
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-09-28 19:39:30 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,23 +5,16 @@
  * @b - pointing to a string of 0 and 1
  * Return: the converted number, or 0
  */
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0;
-
-	if (b == NULL)
-		return (0);
-
-	while (*b)
+	int i;
+	unsigned int digit = 0;
+	if (!b)return (0);
+	for (i = 0; b[i]; i++)
 	{
-		if (*b == '1')
-			dec = (dec << 1) | 1;
-		else if (*b == '0')
-			dec <<= 1;
-		else
-			return (0);
-		b++;
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		digit = 2 * digit + (b[i] - '0');
 	}
-
-	return (dec);
+return (digit);
 }

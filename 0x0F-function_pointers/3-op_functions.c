560b53f4009f3f6b4e1ba1504b62bb89f5942e84
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-03 00:59:31 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,60 +1,66 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
-* op_add - calculates the sum of two integers
-* @a: first integer
-* @b: second integer
-*
-* Return: sum of a and b
-*/
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-* op_sub - calculates the difference of two integers
-* @a: first integer
-* @b: second integer
-*
-* Return: difference of a and b
-*/
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-* op_mul - calculates the product of two integers
-* @a: first integer
-* @b: second integer
-*
-* Return: product of a and b
-*/
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-* op_div - calculates the division of two integers
-* @a: first integer
-* @b: second integer
-*
-* Return: result of the division of a and b
-*/
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
-* op_mod - calculates the remainder of the division of two integers
-* @a: first integer
-* @b: second integer
-*
-* Return: remainder of a divided b
-*/
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);

da22b831f01485e6d7cc2de26c4154a03f6074f4
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-03 01:14:48 +0200

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * free_dog - fuction frees the memory
- * @d: typedef of structure
- * Return: void
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
 }

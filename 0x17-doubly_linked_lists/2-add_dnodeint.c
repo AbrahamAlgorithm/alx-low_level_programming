21364008959fc3cbd25fdb95131e9f271a84c588
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-11-09 13:35:37 +0100

    Add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *  * add_dnodeint - add node to beginning of
+ *   *                linked list
+ *    * @head: the head of a linked list
+ *     * @n: the element to add
+ *      * Return: the new linked list
+ *       */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+		dlistint_t *new;
+
+			new = malloc(sizeof(dlistint_t));
+				if (new == NULL)
+						{
+									return (NULL);
+										}
+					if (*head == NULL)
+							{
+										new->n = n;
+												new->next = NULL;
+														new->prev = NULL;
+																*head = new;
+																		return (*head);
+																			}
+						new->n = n;
+							(*head)->prev = new;
+								new->next = *head;
+									new->prev = NULL;
+										*head = new;
+											return (*head);
+}

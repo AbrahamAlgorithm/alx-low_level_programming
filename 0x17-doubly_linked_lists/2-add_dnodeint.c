f62c31047adb6286e5c3e181ac1f9c18cfd9abf9
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-10 17:35:46 +0100

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,33 +1,26 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - add node to beginning of
- *                linked list
- * @head: the head of a linked list
- * @n: the element to add
- * Return: the new linked list
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: double pointer to the head of the list
+ * @n: data to be added
+ *
+ * Return: he address of the new element, or NULL if it failed
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new_node;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
-	if (*head == NULL)
-	{
-		new->n = n;
-		new->next = NULL;
-		new->prev = NULL;
-		*head = new;
-		return (*head);
-	}
-	new->n = n;
-	(*head)->prev = new;
-	new->next = *head;
-	new->prev = NULL;
-	*head = new;
-	return (*head);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (new_node);
 }

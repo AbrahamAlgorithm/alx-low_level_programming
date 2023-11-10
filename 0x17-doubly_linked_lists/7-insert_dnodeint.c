f060c57a247a4845b0f0cee71d4cc7477983a70d
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-10 17:33:36 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,40 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
- * @h: A pointer to the head of the dlistint_t list.
- * @idx: The position to insert the new node.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * insert_dnodeint_at_index - insert node at index
+ * @h: the head:
+ * @idx: the index
+ * @n: what to add
+ * Return: new nodes
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	dlistint_t *new, *last, *back;
+	unsigned int r = 0;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
+	if (*h == NULL)
 	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-	}
-
-	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
-
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
+	{
 		return (NULL);
-
-	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
-
-	return (new);
+	}
+	last = *h;
+	while (last)
+	{
+		if (idx == r)
+		{
+			new->n = n;
+			back = last->prev;
+			new->next = last;
+			new->prev = back;
+			back->next = new;
+			return (*h);
+		}
+		last = last->next;
+		r++;
+	}
+	return (NULL);
 }
